b8257859a6293f5d1fa788d44c3814b75d11a4e2
jeff skoldberg <jeff@gmdata.co>
2023-08-23 14:06:17 -0400

    emerald-target = matillion

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -3,7 +3,10 @@
 {%- set default_schema = target.schema -%}
     
     {% if target.name == 'qa' and custom_schema_name is not none -%}
-    qa_{{ custom_schema_name | trim }}
+        qa_{{ custom_schema_name | trim }}
+    
+    {% elif target.name == 'emerald-target' and custom_schema_name is not none -%}
+        {{ custom_schema_name | trim }}
     {%- else -%}
         {{ generate_schema_name_for_env(custom_schema_name, node) }}
     {% endif %}
